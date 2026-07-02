902014b7adf19dcd2e7e4b08c1c5038f85ad6610
dipakcode1710 <d.suryavanshi@vitec.co.in>
2026-07-02 10:07:03 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -39,7 +39,7 @@ public class HelloWorld {
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
         panel.setBorder(BorderFactory.createEmptyBorder(40, 40, 40, 40));
 
-        JLabel label = new JLabel("Hello, World!");
+        JLabel label = new JLabel("Hello, World! 1");
         label.setFont(new Font("SansSerif", Font.BOLD, 28));
         label.setForeground(new Color(205, 214, 244));
         label.setAlignmentX(Component.CENTER_ALIGNMENT);
